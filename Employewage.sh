fe736baecf169a1fd74affd5e3cc904ba04e6b52
Dhanush Kumar <dk652003@gmail.com>
2024-01-16 13:44:29 +0530

    Added wages till a condition feature

--- Employewage.sh
+++ Employewage.sh
@@ -1,7 +1,18 @@
-echo "Enter the working hours per month: "
-read a
-echo "Enter the wage per hour: "
-read b
+days=20
+hours=100
+wage=8
 
-wage=$(($a*$b))
-echo $wage
+cur_hr=0
+cur_day=1
+
+result=0
+
+while [ $cur_day -le $days && $cur_hours -le $result ]
+do
+dailywage=$(($hours*8))
+result=$(($dailywage+$result))
+cur_day=$(($cur_day+1))
+cur_hour=$(($cur_hr+1))
+done
+
+echo $result
